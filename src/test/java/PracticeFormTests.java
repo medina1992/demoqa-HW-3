c02524564c8ed137b9162291794b64f620934a5c
Medina_Akhundova <medina.mstudio@gmail.com>
2025-04-13 14:22:23 +0300

    add PracticeFormTests-done

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -21,12 +21,13 @@ public class PracticeFormTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
-        Configuration.timeout = 5000;
+
     }
     @Test
     void fillFormTests() {
 
         open("/automation-practice-form");
+
         $("#firstName").setValue("Medina");
         $("#lastName").setValue("Akhundova");
         $("#userEmail").setValue("medina@gmail.com");
@@ -36,13 +37,32 @@ public class PracticeFormTests {
         $("[id=dateOfBirthInput]").sendKeys(Keys.CONTROL + "A");
         $("#dateOfBirthInput").sendKeys(SPACE);
         $("#dateOfBirthInput").setValue("09 Jul 1992").pressEnter();
-        $("#subjectsInput").setValue("QA");
+        $("#subjectsInput").setValue("Maths").pressEnter();
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("foto.jpg");
         $("#currentAddress").setValue("My address");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
+
         $("#submit"). click();
+
+
+
+
+//Response result
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Medina Akhundova"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("medina@gmail.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9967962177"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("09 July,1992"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("foto.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("My address"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+
+        $("#closeLargeModal"). click();
 
 
             }
