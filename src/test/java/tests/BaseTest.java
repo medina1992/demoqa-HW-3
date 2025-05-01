e9ab07594508d6710651c5f878028c8590c3f702
medina1992 <medina.mstudio@gmail.com>
2025-05-01 14:45:35 +0400

    Homework -7 add Faker

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
+    protected static Faker faker;
+
     @BeforeAll
     static void setup() {
         Configuration.browserSize = "1920x1080";
@@ -16,6 +19,7 @@ public class BaseTest {
 
     @AfterEach
     public void tearDown() {
+
         closeWebDriver();
     }
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -3,36 +3,48 @@ package tests;
 import org.junit.jupiter.api.Test;
 import page.PracticeFormPage;
 
-public class PracticeFormTests extends BaseTest{
+public class PracticeFormTests extends BaseTest {
 
     PracticeFormPage formPage = new PracticeFormPage();
     // позитивный тестовый сценарий E2E
 
     @Test
     void fillFormTest() {
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String phone = faker.phoneNumber().subscriberNumber(10);
+        String address = faker.address().fullAddress();
+        String gender = "Female";
+        String birthDate = "09 Jul 1992";
+        String subject = "Maths";
+        String hobby = "Music";
+        String picture = "foto.jpg";
+        String state = "NCR";
+        String city = "Delhi";
         formPage.removeBanners()
-                .setFirstName("Medina")
-                .setLastName("Akhundova")
-                .setEmail("medina@gmail.com")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
                 .selectGender("Female")
-                .setPhone("9967962177")
+                .setPhone(phone)
                 .setBirthDate("09 Jul 1992")
                 .setSubject("Maths")
                 .selectHobby("Music")
                 .uploadFile("foto.jpg")
-                .setAddress("My address")
+                .setAddress(address)
                 .selectState("NCR")
                 .selectCity("Delhi")
                 .submitForm()
-                .verifyResult("Student Name", "Medina Akhundova")
-                .verifyResult("Student Email", "medina@gmail.com")
-                .verifyResult("Gender", "Female")
-                .verifyResult("Mobile", "9967962177")
+                .verifyResult("Student Name", firstName + " " + lastName)
+                .verifyResult("Student Email", email)
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", phone)
                 .verifyResult("Date of Birth", "09 July,1992")
                 .verifyResult("Subjects", "Maths")
                 .verifyResult("Hobbies", "Music")
                 .verifyResult("Picture", "foto.jpg")
-                .verifyResult("Address", "My address")
+                .verifyResult("Address", address)
                 .verifyResult("State and City", "NCR Delhi");
 
         formPage.closeModal();
@@ -41,9 +53,12 @@ public class PracticeFormTests extends BaseTest{
     //негативный тестовый сценарий (не заполняем обязательные поля)
     @Test
     void negativeFormTest_MissingRequiredFields() {
+        String lastName = faker.name().lastName();
+        String Email = faker.internet().emailAddress();
+
         formPage.removeBanners()
-                .setLastName("Akhundova")
-                .setEmail("medina@gmail.com")
+                .setLastName(lastName)
+                .setEmail(Email)
                 .selectGender("Female")
                 .submitForm();
 
@@ -53,15 +68,19 @@ public class PracticeFormTests extends BaseTest{
     //позитивная проверка по минилаьным количествам вводимых обязательных параметров
     @Test
     void formShouldBeSubmittedWithMinimalRequiredFields() {
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String phone = faker.phoneNumber().phoneNumber();
+
         formPage.removeBanners()
                 .setFirstName("Medina")
                 .setLastName("Akhundova")
                 .selectGender("Female")
                 .setPhone("9967962177")
                 .submitForm()
-                .verifyResult("Student Name", "Medina Akhundova")
+                .verifyResult("Student Name", firstName + " " + lastName)
                 .verifyResult("Gender", "Female")
-                .verifyResult("Mobile", "9967962177");
+                .verifyResult("Mobile", phone);
 
         formPage.closeModal();
     }
