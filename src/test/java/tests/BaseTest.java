472062bd19270896edcb04616d37ba19ac0044b3
Medina_Akhundova <medina.mstudio@gmail.com>
2025-05-21 23:51:42 +0300

    fix9

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,10 +5,8 @@ import com.codeborne.selenide.WebDriverRunner;
 import helpers.AllureAttachments;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import page.PracticeFormPage;
 
 import java.util.Map;
 
@@ -23,31 +21,37 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-
     }
 
     @AfterEach
-    public void addAttachments() {
-        AllureAttachments.screenshotAs("Last screenshot");
-        AllureAttachments.pageSource();
-        AllureAttachments.browserConsoleLogs();
-        AllureAttachments.browserConsoleLogs();
+    void addAllureAttachments() {
+        try {
+            if (WebDriverRunner.hasWebDriverStarted()) {
+                AllureAttachments.screenshotAs("Last screenshot");
+                AllureAttachments.pageSource();
+                AllureAttachments.browserConsoleLogs();
 
+                try {
+                    RemoteWebDriver driver = (RemoteWebDriver) WebDriverRunner.getWebDriver();
+                    String sessionId = driver.getSessionId().toString();
+                    AllureAttachments.attachVideo(sessionId);
+                } catch (Exception e) {
+                    System.out.println("Video attachment skipped: " + e.getMessage());
+                }
 
-            try {
-                RemoteWebDriver driver = (RemoteWebDriver) WebDriverRunner.getWebDriver();
-                String sessionId = driver.getSessionId().toString();
-                AllureAttachments.attachVideo(sessionId);
-            } catch (Exception e) {
-                System.out.println("Video attachment skipped: " + e.getMessage());
+                closeWebDriver();
+            } else {
+                System.out.println("WebDriver not started. Skipping attachments.");
             }
-
-        closeWebDriver(); // закрываем браузер
+        } catch (Exception e) {
+            System.out.println("Error during attachments: " + e.getMessage());
+        }
     }
 }
