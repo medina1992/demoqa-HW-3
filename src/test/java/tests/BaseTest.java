13e72f46f49e8e25bb9cca6f1c068848fc641b4b
Medina_Akhundova <medina.mstudio@gmail.com>
2025-05-21 23:57:14 +0300

    fix10

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,57 +1,42 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import helpers.AllureAttachments;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
 
     @BeforeAll
-    static void setup() {
+    static void setup() throws Exception {
         Configuration.remote = "http://selenoid.autotests.cloud:8080/wd/hub";
-        Configuration.browser = "chrome";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
 
+        // Создаем уникальную временную папку для user-data-dir
+        Path userDataDir = Files.createTempDirectory("chrome-user-data-");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--disable-dev-shm-usage",
+                "--no-sandbox",
+                "--user-data-dir=" + userDataDir.toAbsolutePath().toString()
+        );
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        // Включаем видео и VNC в Selenoid
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
         Configuration.browserCapabilities = capabilities;
-    }
-
-    @AfterEach
-    void addAllureAttachments() {
-        try {
-            if (WebDriverRunner.hasWebDriverStarted()) {
-                AllureAttachments.screenshotAs("Last screenshot");
-                AllureAttachments.pageSource();
-                AllureAttachments.browserConsoleLogs();
-
-                try {
-                    RemoteWebDriver driver = (RemoteWebDriver) WebDriverRunner.getWebDriver();
-                    String sessionId = driver.getSessionId().toString();
-                    AllureAttachments.attachVideo(sessionId);
-                } catch (Exception e) {
-                    System.out.println("Video attachment skipped: " + e.getMessage());
-                }
-
-                closeWebDriver();
-            } else {
-                System.out.println("WebDriver not started. Skipping attachments.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error during attachments: " + e.getMessage());
-        }
     }
 }
